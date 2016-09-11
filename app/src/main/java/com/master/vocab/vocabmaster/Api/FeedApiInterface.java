ddf4afdc8b24e9f21baf6de5cb351dcb8a3f94cf
sahil deswal <ssahilsahil798@gmail.com>
2016-09-12 02:29:50 +0530

    api integrated only ui left now

--- app/src/main/java/com/master/vocab/vocabmaster/Api/FeedApiInterface.java
+++ app/src/main/java/com/master/vocab/vocabmaster/Api/FeedApiInterface.java
@@ -1,7 +1,33 @@
 package com.master.vocab.vocabmaster.Api;
 
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Path;
+
 /**
  * Created by sahildeswal on 02/09/16.
  */
-public class FeedApiInterface {
+public interface FeedApiInterface {
+
+    @FormUrlEncoded
+    @POST("user/auth/login")
+    public void manualLogin(@Field("username") String username, @Field("password") String password);
+
+    @FormUrlEncoded
+    @POST("user/register")
+    public void manualRegister(@Field("username") String username, @Field("password") String password);
+
+    @GET("/cardstatus")
+    public void getUserCardStatus();
+
+    @GET("/wordstatus")
+    public void getCategoryWordStatus(@Path("card_status") String card_status);
+
+    @FormUrlEncoded
+    @POST("/wordstatus")
+    public void changeWordStatus(@Field("id") String word_id, @Field("word_status") String word_status);
+
+
 }
